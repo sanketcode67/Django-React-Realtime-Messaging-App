48c5fe6c6acbbc212060b299d83b51ba150ac9de
Sanket De <sanket.de.24.1@mountblue.tech>
2023-08-01 10:33:04 +0530

    added CORS allowed host

--- messagingAppBackend/messagingAppBackend/settings.py
+++ messagingAppBackend/messagingAppBackend/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'paralaxiom_auth',
-    'app',
+    'corsheaders',
 ]
 
 
@@ -65,6 +65,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -93,6 +94,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'messagingAppBackend.wsgi.application'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 
 # Database

65e3fa163baa88ea4a0d617e489eb7d739c47f47
Sanket De <sanket.de.24.1@mountblue.tech>
2023-08-04 13:48:39 +0530

    added changed password view

--- messagingAppBackend/paralaxiom_auth/urls.py
+++ messagingAppBackend/paralaxiom_auth/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('register/', views.userRegistrationView, name='user-register'),
     path('login/', views.userLoginView, name='user-login'),
     path('logout/', views.logout_view, name='user-logout'),
+    path('change-password/', views.change_password_view, name='user-change-password'),
+
     path('user/all/', views.allUsersView, name='all-friend'),
 
 
--- messagingAppBackend/paralaxiom_auth/views.py
+++ messagingAppBackend/paralaxiom_auth/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import make_password
 from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer, UserListSerializer
 from rest_framework import status
@@ -79,6 +80,37 @@ def logout_view(request):
     except Exception as e:
         logger.error(f"Error during logout: {e}")
         return Response({'error': 'Something went wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+# change password view
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def change_password_view(request):
+
+    #get the user
+    user = request.user
+
+    # get the data from the response
+    old_password = request.data.get('old_password')
+    new_password = request.data.get('new_password')
+
+    # Check if the old and new passwords are the same
+    if old_password == new_password:
+        return Response({'error': 'New password cannot be the same as the old password'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+    # Check if the old password matches the user's current password
+    if not user.check_password(old_password):
+        logging.info("old password is incorrect")
+        return Response({'error': 'Old password is incorrect'}, status=status.HTTP_400_BAD_REQUEST)
+
+     # Set the new password for the user
+    user.password = make_password(new_password)
+    user.save()
+
+    logging.info(f"Password changed successfully by username {user}")
+    return Response({'message': 'Password changed successfully'}, status=status.HTTP_200_OK)    
+
 
 
 
